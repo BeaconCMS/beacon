77d5e8b6f26133a49a56084091b71721ec50439b
Felipe Renan <feelipe.renan@gmail.com>
2022-07-15 18:35:43 +0100

    Update lib/beacon/application.ex

--- lib/beacon/application.ex
+++ lib/beacon/application.ex
@@ -17,7 +17,6 @@ defmodule Beacon.Application do
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
-
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Beacon.Supervisor]
     Supervisor.start_link(children, opts)
