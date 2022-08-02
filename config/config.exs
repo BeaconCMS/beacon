8e89ff2e0e278690c3826ed309456a604c799878
Felipe Renan <feelipe.renan@gmail.com>
2022-08-02 23:47:22 +0100

    Add config/test.exs

--- config/config.exs
+++ config/config.exs
@@ -1,19 +1,5 @@
 import Config
 
-# This required by Phoenix
-config :phoenix, :json_library, Jason
-
-config :logger, level: :error
-
-config :beacon, Beacon.Repo,
-  database: "beacon_test",
-  password: "postgres",
-  pool: Ecto.Adapters.SQL.Sandbox,
-  username: "postgres",
-  ownership_timeout: 1_000_000_000
-
-config :beacon,
-  ecto_repos: [Beacon.Repo]
-
-# set default DataSource implementation for tests.
-config :beacon, :data_source, DummyApp.BeaconDataSource
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{config_env()}.exs"
