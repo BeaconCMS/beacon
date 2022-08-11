e37e50d938cbb93646a12b538bd8005e1ae57b03
Felipe Renan <feelipe.renan@gmail.com>
2022-08-11 22:38:23 +0100

    Improve error for path not found
    Closes https://github.com/BeaconCMS/beacon/issues/21

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,7 @@
 import Config
 
+config :beacon, ecto_repos: [Beacon.Repo]
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
--- test/support/dummy_app/beacon_data_source.ex
+++ test/support/dummy_app/beacon_data_source.ex
@@ -2,4 +2,5 @@ defmodule DummyApp.BeaconDataSource do
   @behaviour Beacon.DataSource.Behaviour
 
   def live_data("my_site", ["home"], _params), do: %{vals: ["first", "second", "third"]}
+  def live_data("my_site", ["no_page_match"], _params), do: []
 end
