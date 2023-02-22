116ff127cf61b5e849b50871670015a22ef32d7d
Leandro Cesquini Pereira <leandro@leandro.io>
2023-02-22 17:37:10 -0500

    meta tags migration - guard against nil rows

--- priv/repo/migrations/20230216184343_change_layouts_meta_tags_to_array_map.exs
+++ priv/repo/migrations/20230216184343_change_layouts_meta_tags_to_array_map.exs
@@ -14,27 +14,29 @@ defmodule Beacon.Repo.Migrations.ChangeLayoutsMetaTagsToArrayMap do
     types = %{id: :binary_id, old_meta_tags: :map}
     result = repo().query!(layout_meta_tags)
 
-    # for each row, query old meta tags, transform it, and store the transformed values as array
-    for row <- result.rows do
-      %{id: id, old_meta_tags: old_meta_tags} = repo().load(types, {result.columns, row})
-      {:ok, id} = Ecto.UUID.dump(id)
+    unless result.rows do
+      # for each row, query old meta tags, transform it, and store the transformed values as array
+      for row <- result.rows do
+        %{id: id, old_meta_tags: old_meta_tags} = repo().load(types, {result.columns, row})
+        {:ok, id} = Ecto.UUID.dump(id)
 
-      new_meta_tags =
-        Enum.map(old_meta_tags, fn {key, value} ->
-          %{"name" => key, "content" => value}
+        new_meta_tags =
+          Enum.map(old_meta_tags, fn {key, value} ->
+            %{"name" => key, "content" => value}
+          end)
+
+        execute(fn ->
+          repo().query!(
+            """
+            UPDATE beacon_layouts
+               SET array_meta_tags = $1
+             WHERE id = $2
+            """,
+            [new_meta_tags, id],
+            log: :info
+          )
         end)
-
-      execute(fn ->
-        repo().query!(
-          """
-          UPDATE beacon_layouts
-             SET array_meta_tags = $1
-           WHERE id = $2
-          """,
-          [new_meta_tags, id],
-          log: :info
-        )
-      end)
+      end
     end
 
     # remove old meta tags
