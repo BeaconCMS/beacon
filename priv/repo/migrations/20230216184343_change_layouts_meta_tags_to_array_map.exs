b6c355eef2d108d5b1270235f581677ae35f30bb
Leandro Cesquini Pereira <leandro@leandro.io>
2023-02-23 10:27:03 -0500

    make migration more resilient

--- priv/repo/migrations/20230216184343_change_layouts_meta_tags_to_array_map.exs
+++ priv/repo/migrations/20230216184343_change_layouts_meta_tags_to_array_map.exs
@@ -7,36 +7,39 @@ defmodule Beacon.Repo.Migrations.ChangeLayoutsMetaTagsToArrayMap do
       add :array_meta_tags, {:array, :map}
     end
 
-    layout_meta_tags = """
+    query_layout_meta_tags = """
       SELECT id, meta_tags AS old_meta_tags FROM beacon_layouts
     """
 
     types = %{id: :binary_id, old_meta_tags: :map}
-    result = repo().query!(layout_meta_tags)
 
-    unless result.rows do
-      # for each row, query old meta tags, transform it, and store the transformed values as array
-      for row <- result.rows do
-        %{id: id, old_meta_tags: old_meta_tags} = repo().load(types, {result.columns, row})
-        {:ok, id} = Ecto.UUID.dump(id)
+    case repo().query(query_layout_meta_tags) do
+      {:ok, result} ->
+        # for each row, query old meta tags, transform it, and store the transformed values as array
+        for row <- result.rows do
+          %{id: id, old_meta_tags: old_meta_tags} = repo().load(types, {result.columns, row})
+          {:ok, id} = Ecto.UUID.dump(id)
 
-        new_meta_tags =
-          Enum.map(old_meta_tags, fn {key, value} ->
-            %{"name" => key, "content" => value}
+          new_meta_tags =
+            Enum.map(old_meta_tags, fn {key, value} ->
+              %{"name" => key, "content" => value}
+            end)
+
+          execute(fn ->
+            repo().query!(
+              """
+              UPDATE beacon_layouts
+                 SET array_meta_tags = $1
+               WHERE id = $2
+              """,
+              [new_meta_tags, id],
+              log: :info
+            )
           end)
+        end
 
-        execute(fn ->
-          repo().query!(
-            """
-            UPDATE beacon_layouts
-               SET array_meta_tags = $1
-             WHERE id = $2
-            """,
-            [new_meta_tags, id],
-            log: :info
-          )
-        end)
-      end
+      _ ->
+        :skip
     end
 
     # remove old meta tags
