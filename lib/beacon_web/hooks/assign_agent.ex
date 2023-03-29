c9efc58bd0138edd080fcafef2ab734eaa0dc77f
Christian Trosclair <christian.trosclair@gmail.com>
2023-03-29 12:58:47 -0400

    better documentation

--- lib/beacon_web/hooks/assign_agent.ex
+++ lib/beacon_web/hooks/assign_agent.ex
@@ -2,7 +2,10 @@ defmodule BeaconWeb.Hooks.AssignAgent do
   @moduledoc """
     Assigns the agent on the socket to be used by `Beacon.Authorization`.
 
-    To enable Authorization you must set a session key `authorization_bootstrap` with a value that will be consumed by `Beacon.Authorization`.
+    To enable Authorization you must set a session key/value `authorization_bootstrap`.
+    Beacon does not in itself depend on the contents of `authorization_bootstrap`.
+    This is to be consumed by your own `Beacon.Authorization` implementation.
+
     It is presumed you will have already authenticated the agent with your own hook.
     See `Beacon.Router.beacon_admin/2` for details on adding hooks.
 
