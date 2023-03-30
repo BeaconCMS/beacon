be30b700399a4840dc5d70f426be1f0e45f5838e
Leandro Cesquini Pereira <leandro@leandro.io>
2023-03-30 14:25:34 -0400

    remove debug code

--- lib/beacon_web/admin/hooks/authorized.ex
+++ lib/beacon_web/admin/hooks/authorized.ex
@@ -11,19 +11,4 @@ defmodule BeaconWeb.Admin.Hooks.Authorized do
       {:halt, redirect(socket, to: redirect_to)}
     end
   end
-
-  def on_mount(%Beacon.Admin.MediaLibrary.Asset{} = context, _params, _session, socket) do
-    if Beacon.Authorization.authorized?(socket.assigns.agent, :index, context) do
-      {:cont, socket}
-    else
-      redirect_to = Beacon.Router.beacon_admin_path(socket, "/")
-      {:halt, redirect(socket, to: redirect_to)}
-    end
-  end
-
-  def handle_params(a, b, socket) do
-    dbg(a)
-    dbg(b)
-    {:cont, socket}
-  end
 end
