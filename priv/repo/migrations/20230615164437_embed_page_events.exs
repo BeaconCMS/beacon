73ab07e8c01c46b46f13aa815118fa2abd3b330c
Leandro Pereira <leandro@leandro.io>
2023-06-26 11:37:54 -0400

    fix migrations

--- priv/repo/migrations/20230615164437_embed_page_events.exs
+++ priv/repo/migrations/20230615164437_embed_page_events.exs
@@ -1,45 +1,65 @@
 defmodule Beacon.Repo.Migrations.EmbedPageEvents do
   use Ecto.Migration
 
+  defp page_ids do
+    query = """
+    SELECT DISTINCT page_id FROM beacon_page_events
+    """
+
+    case repo().query(query, [], log: :info) do
+      {:ok, result} ->
+        for row <- result.rows do
+          %{page_id: page_id} = repo().load(%{page_id: :binary_id}, {result.columns, row})
+          {:ok, page_id} = Ecto.UUID.dump(page_id)
+          page_id
+        end
+
+      _ ->
+        []
+    end
+  end
+
+  defp events_for_page(page_id) do
+    query = """
+    SELECT id, code, event_name FROM beacon_page_events WHERE page_id = $1
+    """
+
+    case repo().query(query, [page_id], log: :info) do
+      {:ok, result} ->
+        types = %{id: :binary_id, code: :string, event_name: :string}
+
+        Enum.map(result.rows, fn row ->
+          %{id: id, code: code, event_name: event_name} = repo().load(types, {result.columns, row})
+          %{id: id, code: code, name: event_name}
+        end)
+
+      _ ->
+        []
+    end
+  end
+
+  defp update_page_events(page_id, events) do
+    execute(fn ->
+      repo().query!(
+        """
+        UPDATE beacon_pages
+           SET events = $1
+         WHERE id = $2
+        """,
+        [events, page_id],
+        log: :info
+      )
+    end)
+  end
+
   def up do
     alter table("beacon_pages") do
       add :events, :map, comment: "liveview events consumed by handle_event"
     end
 
-    query_page_events = """
-    SELECT id, page_id, code, event_name FROM beacon_page_events
-    """
-
-    types = %{id: :binary_id, page_id: :binary_id, code: :string, event_name: :string}
-
-    case repo().query(query_page_events) do
-      {:ok, result} ->
-        for row <- result.rows do
-          %{id: id, page_id: page_id, code: code, event_name: name} = repo().load(types, {result.columns, row})
-          {:ok, page_id} = Ecto.UUID.dump(page_id)
-
-          embedded_event = %{
-            id: id,
-            code: code,
-            name: name
-          }
-
-          # TODO: group events
-          execute(fn ->
-            repo().query!(
-              """
-              UPDATE beacon_pages
-                 SET events = $1
-               WHERE id = $2
-              """,
-              [embedded_event, page_id],
-              log: :info
-            )
-          end)
-        end
-
-      _ ->
-        :skip
+    for page_id <- page_ids() do
+      events = events_for_page(page_id)
+      update_page_events(page_id, events)
     end
 
     drop table("beacon_page_events")
--- priv/repo/migrations/20230615204709_embed_page_helpers.exs
+++ priv/repo/migrations/20230615204709_embed_page_helpers.exs
@@ -1,46 +1,65 @@
 defmodule Beacon.Repo.Migrations.EmbedPageHelpers do
   use Ecto.Migration
 
+  defp page_ids do
+    query = """
+    SELECT DISTINCT page_id FROM beacon_page_helpers
+    """
+
+    case repo().query(query, [], log: :info) do
+      {:ok, result} ->
+        for row <- result.rows do
+          %{page_id: page_id} = repo().load(%{page_id: :binary_id}, {result.columns, row})
+          {:ok, page_id} = Ecto.UUID.dump(page_id)
+          page_id
+        end
+
+      _ ->
+        []
+    end
+  end
+
+  defp helpers_for_page(page_id) do
+    query = """
+    SELECT id, code, helper_name FROM beacon_page_helpers WHERE page_id = $1
+    """
+
+    case repo().query(query, [page_id], log: :info) do
+      {:ok, result} ->
+        types = %{id: :binary_id, code: :string, helper_name: :string}
+
+        Enum.map(result.rows, fn row ->
+          %{id: id, code: code, helper_name: helper_name} = repo().load(types, {result.columns, row})
+          %{id: id, code: code, name: helper_name}
+        end)
+
+      _ ->
+        []
+    end
+  end
+
+  defp update_page_helpers(page_id, helpers) do
+    execute(fn ->
+      repo().query!(
+        """
+        UPDATE beacon_pages
+           SET helpers = $1
+         WHERE id = $2
+        """,
+        [helpers, page_id],
+        log: :info
+      )
+    end)
+  end
+
   def up do
     alter table("beacon_pages") do
       add :helpers, :map
     end
 
-    query_page_helpers = """
-    SELECT id, page_id, code, helper_name, helper_args FROM beacon_page_helpers
-    """
-
-    types = %{id: :binary_id, page_id: :binary_id, code: :string, helper_name: :string, helper_args: :string}
-
-    case repo().query(query_page_helpers) do
-      {:ok, result} ->
-        for row <- result.rows do
-          %{id: id, page_id: page_id, code: code, helper_name: name, helper_args: args} = repo().load(types, {result.columns, row})
-          {:ok, page_id} = Ecto.UUID.dump(page_id)
-
-          embedded_helper = %{
-            id: id,
-            code: code,
-            name: name,
-            args: args
-          }
-
-          # TODO: group helpers
-          execute(fn ->
-            repo().query!(
-              """
-              UPDATE beacon_pages
-                 SET helpers = $1
-               WHERE id = $2
-              """,
-              [embedded_helper, page_id],
-              log: :info
-            )
-          end)
-        end
-
-      _ ->
-        :skip
+    for page_id <- page_ids() do
+      helpers = helpers_for_page(page_id)
+      update_page_helpers(page_id, helpers)
     end
 
     drop table("beacon_page_helpers")
