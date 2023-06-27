b26f2526c6dc82814f78b3419fefd58d36680f38
Christian Trosclair <christian.trosclair@gmail.com>
2023-06-27 15:47:47 -0500

    backend arity fix

--- lib/beacon/admin/media_library/backend.ex
+++ lib/beacon/admin/media_library/backend.ex
@@ -8,16 +8,28 @@ defmodule Beacon.Admin.MediaLibrary.Backend do
   @spec validate_for_delivery(Beacon.Admin.MediaLibrary.UploadMetadata.t()) :: Beacon.Admin.MediaLibrary.UploadMetadata.t()
   def validate_for_delivery(%UploadMetadata{} = metadata) do
     Enum.reduce(metadata.config.validations, metadata, fn
-      validation, {md, config} -> validation.(md, config)
       validation, md -> validation.(md)
+    end)
+  end
+
+  @spec validate_for_delivery({Beacon.Admin.MediaLibrary.UploadMetadata.t(), any()}) :: Beacon.Admin.MediaLibrary.UploadMetadata.t()
+  def validate_for_delivery({%UploadMetadata{} = metadata, config}) do
+    Enum.reduce(metadata.config.validations, metadata, fn
+      validation, md -> validation.(md, config)
     end)
   end
 
   @spec send_to_cdns(Beacon.Admin.MediaLibrary.UploadMetadata.t()) :: Beacon.Admin.MediaLibrary.UploadMetadata.t()
   def send_to_cdns(%UploadMetadata{} = metadata) do
     Enum.reduce(metadata.config.backends, metadata, fn
-      backend, {md, config} -> backend.send_to_cdn(md, config)
       backend, md -> backend.send_to_cdn(md)
+    end)
+  end
+
+  @spec send_to_cdns({Beacon.Admin.MediaLibrary.UploadMetadata.t(), any()}) :: Beacon.Admin.MediaLibrary.UploadMetadata.t()
+  def send_to_cdns({%UploadMetadata{} = metadata, config}) do
+    Enum.reduce(metadata.config.backends, metadata, fn
+      backend, md -> backend.send_to_cdn(md, config)
     end)
   end
 end
