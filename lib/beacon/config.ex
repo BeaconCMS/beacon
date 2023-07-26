7a63fa2a6d142796e4aed7a34e948c9007b6c4b3
Christian Trosclair <christian.trosclair@gmail.com>
2023-07-26 12:33:56 -0400

    appease the formatter

--- lib/beacon/config.ex
+++ lib/beacon/config.ex
@@ -460,7 +460,8 @@ defmodule Beacon.Config do
   defp validate_accept_extension(<<46, _rest::binary>> = media_type), do: media_type
 
   # catchall
-  defp validate_accept_extension(media_type), do: raise Beacon.LoaderError, "`#{media_type}` does not appear to be a media type, extensions must begin with a `.`"
+  defp validate_accept_extension(media_type),
+    do: raise(Beacon.LoaderError, "`#{media_type}` does not appear to be a media type, extensions must begin with a `.`")
 
   defp process_assets_config(allowed_media_accept_types, assigned_assets) do
     Enum.reduce(
@@ -509,7 +510,7 @@ defmodule Beacon.Config do
   end
 
   defp ensure_backend_for_extension(_configs, extension_without_leading_dot),
-    do: raise Beacon.LoaderError, "`#{extension_without_leading_dot}` does not appear to be a media type, extensions must begin with a `.`"
+    do: raise(Beacon.LoaderError, "`#{extension_without_leading_dot}` does not appear to be a media type, extensions must begin with a `.`")
 
   defp raise_invalid_media_type(media_type) do
     raise(Beacon.LoaderError, "Unknown Media type: #{media_type}")
