bdf469e5f7e8249773a1f8d649e0fc06eb50d2f0
APB9785 <74077809+APB9785@users.noreply.github.com>
2023-07-27 11:29:32 -0500

    Test data_source, mixfile, seeds

--- test/mix/tasks/install_test_alt.exs
+++ test/mix/tasks/install_test_alt.exs
@@ -10,6 +10,9 @@ defmodule Mix.Tasks.Beacon.InstallTest do
   @prod_path "config/prod.exs"
   @application_path "lib/my_app/application.ex"
   @router_path "lib/my_app_web/router.ex"
+  @data_source_path "lib/my_app/beacon_data_source.ex"
+  @mixfile_path "mix.exs"
+  @seeds_path "priv/repo/beacon_seeds.exs"
 
   setup do
     create_sample_project()
@@ -315,6 +318,86 @@ defmodule Mix.Tasks.Beacon.InstallTest do
                  end
                end
                """
+
+               assert File.read!(@mixfile_path) ==
+                """
+                defmodule MyApp.MixProject do
+                  use Mix.Project
+
+                  def project do
+                    [
+                      app: :my_app,
+                      version: "0.1.0",
+                      elixir: "~> 1.14",
+                      elixirc_paths: elixirc_paths(Mix.env()),
+                      start_permanent: Mix.env() == :prod,
+                      aliases: aliases(),
+                      deps: deps()
+                    ]
+                  end
+
+                  # Configuration for the OTP application.
+                  #
+                  # Type `mix help compile.app` for more information.
+                  def application do
+                    [
+                      mod: {MyApp.Application, []},
+                      extra_applications: [:logger, :runtime_tools]
+                    ]
+                  end
+
+                  # Specifies which paths to compile per environment.
+                  defp elixirc_paths(:test), do: ["lib", "test/support"]
+                  defp elixirc_paths(_), do: ["lib"]
+
+                  # Specifies your project dependencies.
+                  #
+                  # Type `mix help deps` for examples and options.
+                  defp deps do
+                    [
+                      {:phoenix, "~> 1.7.6"},
+                      {:phoenix_ecto, "~> 4.4"},
+                      {:ecto_sql, "~> 3.10"},
+                      {:postgrex, ">= 0.0.0"},
+                      {:phoenix_html, "~> 3.3"},
+                      {:phoenix_live_reload, "~> 1.2", only: :dev},
+                      {:phoenix_live_view, "~> 0.19.0"},
+                      {:floki, ">= 0.30.0", only: :test},
+                      {:phoenix_live_dashboard, "~> 0.8.0"},
+                      {:esbuild, "~> 0.7", runtime: Mix.env() == :dev},
+                      {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
+                      {:swoosh, "~> 1.3"},
+                      {:finch, "~> 0.13"},
+                      {:telemetry_metrics, "~> 0.6"},
+                      {:telemetry_poller, "~> 1.0"},
+                      {:gettext, "~> 0.20"},
+                      {:jason, "~> 1.2"},
+                      {:plug_cowboy, "~> 2.5"}
+                    ]
+                  end
+
+                  # Aliases are shortcuts or tasks specific to the current project.
+                  # For example, to install project dependencies and perform other setup tasks, run:
+                  #
+                  #     $ mix setup
+                  #
+                  # See the documentation for `Mix` for more info on aliases.
+                  defp aliases do
+                    [
+                      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
+                      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs", "run priv/repo/beacon_seeds.exs"],
+                      "ecto.reset": ["ecto.drop", "ecto.setup"],
+                      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+                      "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
+                      "assets.build": ["tailwind default", "esbuild default"],
+                      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
+                    ]
+                  end
+                end
+                """
+
+        assert File.exists?(@data_source_path)
+        assert File.exists?(@seeds_path)
     end)
   end
 
