052bb76ab81885eb643c302dbea1bb974a14dbd1
Christian Trosclair <christian.trosclair@gmail.com>
2023-07-28 12:43:17 -0400

    add docs

--- lib/beacon/media_types.ex
+++ lib/beacon/media_types.ex
@@ -1,7 +1,18 @@
 defmodule Beacon.MediaTypes do
-  # Browsers' media types are often out of date,
-  # returning and empty string or a deprecated assignation
-  # which the elixir MIME library no longer supports.
+  @moduledoc """
+    #{__MODULE__} serves as a context to encapsulate business logic
+    around media types that is not handled by the MIME library.
+
+    Primary concerns:
+    - Convenience functions
+    - Functions to handle edge cases between the browser and elxir
+  """
+
+  @doc """
+    Browsers' media types are often out of date,
+    returning and empty string or a deprecated media type
+    which the elixir MIME library no longer supports.
+  """
   def normalize(""), do: nil
 
   def normalize("application/font-woff"), do: "font/woff"
