44d96c3fa65bef97998d167eda8d1ff5988ffef0
Miguel Camba <miguel.camba@gmail.com>
2023-08-01 16:46:59 +0200

    Update dev.exs
    Co-authored-by: Leandro Pereira <leandro@leandro.io>

--- dev.exs
+++ dev.exs
@@ -59,8 +59,6 @@ defmodule SamplePhoenixWeb.Router do
   pipeline :api do
     plug :accepts, ["json"]
     plug BeaconWeb.API.Plug
-    # plug Accent.Plug.Request, default_case: Accent.Case.Snake
-    # plug Accent.Plug.Response, default_case: Accent.Case.Camel, json_codec: Jason
   end
 
   scope "/" do
