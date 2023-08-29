127fb48bb3b382577e4bbceb85f87d0cef33d3f4
APB9785 <74077809+APB9785@users.noreply.github.com>
2023-08-29 10:58:15 -0500

    Test custom error page

--- test/beacon/loader/error_module_loader_test.exs
+++ test/beacon/loader/error_module_loader_test.exs
@@ -1,6 +1,8 @@
 defmodule Beacon.Loader.ErrorModuleLoaderTest do
   use Beacon.DataCase, async: false
 
+  import Beacon.Fixtures
+
   alias Beacon.Loader.ErrorModuleLoader
 
   @site :my_site
@@ -13,14 +15,27 @@ defmodule Beacon.Loader.ErrorModuleLoaderTest do
   test "render default error pages" do
     :ok = Beacon.Loader.populate_layouts(@site)
     :ok = Beacon.Loader.populate_error_pages(@site)
+    error_module = load_error_pages_module(@site)
 
+    assert error_module.render(404) == "Not Found"
+    assert error_module.render(500) == "Internal Server Error"
+  end
+
+  test "render custom error page with layout" do
+    layout = published_layout_fixture(template: "Wow\n<%= @inner_content %>\nLayout", site: @site)
+    error_page = error_page_fixture(layout: layout, template: "Error", site: @site)
+    error_module = load_error_pages_module(@site)
+
+    assert error_module.render(error_page.status) == "Wow\nError\nLayout"
+  end
+
+  defp load_error_pages_module(site) do
     {:ok, module, _ast} =
-      @site
+      site
       |> Beacon.Content.list_error_pages()
       |> Beacon.Repo.preload(:layout)
-      |> ErrorModuleLoader.load_error_pages!(@site)
+      |> ErrorModuleLoader.load_error_pages!(site)
 
-    assert module.render(404) == "Not Found"
-    assert module.render(500) == "Internal Server Error"
+    module
   end
 end
