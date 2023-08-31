7d7c7af9ab4d46793e7aa4b8bea36c1687ce2f3f
Leandro Pereira <leandro@leandro.io>
2023-08-31 11:00:15 -0400

    test ErrorHTML

--- lib/beacon_web/controllers/error_html.ex
+++ lib/beacon_web/controllers/error_html.ex
@@ -4,16 +4,12 @@ defmodule BeaconWeb.ErrorHTML do
 
   alias Beacon.Loader
 
-  def render(template, assigns) do
-    {_, _, %{extra: %{session: %{"beacon_site" => site}}}} = assigns.conn.private.phoenix_live_view
+  def render(<<status_code::binary-size(3), _rest::binary>> = template, %{conn: conn}) do
+    {_, _, %{extra: %{session: %{"beacon_site" => site}}}} = conn.private.phoenix_live_view
     error_module = Loader.error_module_for_site(site)
-
-    status =
-      template
-      |> String.split(".")
-      |> hd()
-      |> String.to_integer()
-
-    {:safe, error_module.render(status)}
+    conn = Plug.Conn.assign(conn, :__site__, site)
+    {:safe, error_module.render(conn, String.to_integer(status_code))}
+  rescue
+    _ -> Phoenix.Controller.status_message_from_template(template)
   end
 end
