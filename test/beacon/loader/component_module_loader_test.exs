ce019aae474b70f479aafbeb1ad44c32ab27a1e2
Alexandre Moreira Xavier <alexandrexaviersm@gmail.com>
2024-03-15 20:55:19 -0300

    Add more tests for different component types

--- test/beacon/loader/component_module_loader_test.exs
+++ test/beacon/loader/component_module_loader_test.exs
@@ -35,14 +35,13 @@ defmodule Beacon.Loader.ComponentModuleLoaderTest do
 
   test "load component without attrs" do
     component_fixture(name: "hello", body: "<h1>Hello</h1>")
-    components = Content.list_components(@site, per_page: :infinity)
+    components = Content.list_components(@site, per_page: :infinity, preloads: [:attrs])
 
     {:ok, mod} = ComponentModuleLoader.load_components(@site, components)
     assert render(mod.hello(%{})) == "<h1>Hello</h1>"
   end
 
-  # TODO: store and retrive attrs in %Beacon.Content.Component{}
-  test "load component with attrs" do
+  test "load component with string attrs" do
     component_fixture(
       name: "say_hello",
       body: "<h1>Hello <%= @first_name %> <%= @last_name %></h1>",
@@ -52,7 +51,89 @@ defmodule Beacon.Loader.ComponentModuleLoaderTest do
     components = Content.list_components(@site, per_page: :infinity, preloads: [:attrs])
 
     {:ok, mod} = ComponentModuleLoader.load_components(@site, components)
-
     assert render(mod.say_hello(%{first_name: "José", last_name: "Valim"})) == "<h1>Hello José Valim</h1>"
+  end
+
+  test "load component with boolean attr" do
+    component_fixture(
+      name: "say_hello",
+      body: """
+      <h1>Hello <%= @first_name %></h1>
+      <h2 :if={@ask_question?}>What's up?</h2>
+      """,
+      attrs: [%{name: "first_name", type: "string"}, %{name: "ask_question?", type: "boolean"}]
+    )
+
+    components = Content.list_components(@site, per_page: :infinity, preloads: [:attrs])
+
+    {:ok, mod} = ComponentModuleLoader.load_components(@site, components)
+    assert render(mod.say_hello(%{first_name: "José", ask_question?: true})) == "<h1>Hello José</h1>\n<h2>What's up?</h2>"
+    assert render(mod.say_hello(%{first_name: "José", ask_question?: false})) == "<h1>Hello José</h1>\n"
+  end
+
+  test "load component with integer attr" do
+    component_fixture(
+      name: "show_versions",
+      body: """
+      <p>Erlang version: <%= @erl_version %></p>
+      <p>Elixir version: <%= @elixir_version %></p>
+      """,
+      attrs: [%{name: "erl_version", type: "integer"}, %{name: "elixir_version", type: "float"}]
+    )
+
+    components = Content.list_components(@site, per_page: :infinity, preloads: [:attrs])
+
+    {:ok, mod} = ComponentModuleLoader.load_components(@site, components)
+    assert render(mod.show_versions(%{erl_version: 26, elixir_version: 1.16})) == "<p>Erlang version: 26</p>\n<p>Elixir version: 1.16</p>"
+  end
+
+  test "load component with atom attr" do
+    component_fixture(
+      name: "say_hello",
+      body: "<p><%= @greeting %>!</p>",
+      attrs: [%{name: "greeting", type: "atom"}]
+    )
+
+    components = Content.list_components(@site, per_page: :infinity, preloads: [:attrs])
+
+    {:ok, mod} = ComponentModuleLoader.load_components(@site, components)
+    assert render(mod.say_hello(%{greeting: :hello})) == "<p>hello!</p>"
+  end
+
+  test "load component with list attr" do
+    component_fixture(
+      name: "advice",
+      body: """
+      <p>Some good programming languages:</p>
+      <ul>
+        <li :for={lang <- @langs}><%= lang %></li>
+      </ul>
+      """,
+      attrs: [%{name: "langs", type: "list"}]
+    )
+
+    components = Content.list_components(@site, per_page: :infinity, preloads: [:attrs])
+
+    {:ok, mod} = ComponentModuleLoader.load_components(@site, components)
+
+    assert render(mod.advice(%{langs: ["Erlang", "Elixir", "Rust"]})) ==
+             "<p>Some good programming languages:</p>\n<ul>\n  <li>Erlang</li><li>Elixir</li><li>Rust</li>\n</ul>"
+  end
+
+  test "load component with map attr" do
+    component_fixture(
+      name: "user_info",
+      body: """
+      <h1>User info:</h1>
+      <p :for={{key, value} <- @user}><%= key %>: <%= value %></p>
+      """,
+      attrs: [%{name: "user", type: "map"}]
+    )
+
+    components = Content.list_components(@site, per_page: :infinity, preloads: [:attrs])
+
+    {:ok, mod} = ComponentModuleLoader.load_components(@site, components)
+
+    assert render(mod.user_info(%{user: %{name: "Joe", age: 20}})) == "<h1>User info:</h1>\n<p>name: Joe</p><p>age: 20</p>"
   end
 end
