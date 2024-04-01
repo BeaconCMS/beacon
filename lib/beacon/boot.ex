8e9b75182ecdbb774f5f242a6b6a4c28bcfd73ad
Leandro Pereira <leandro@leandro.io>
2024-04-01 11:24:31 -0400

    remove :timer.tc
    in favor of telemetry that's on the roadmap

--- lib/beacon/boot.ex
+++ lib/beacon/boot.ex
@@ -23,41 +23,41 @@ defmodule Beacon.Boot do
   def init(config), do: do_init(config)
 
   def do_init(config) do
-    boot = fn ->
-      task_supervisor = task_supervisor(config.site)
+    Logger.info("Beacon.Boot booting site #{config.site}")
 
-      # Layouts and pages depend on the components module so we need to load it first
-      Beacon.Loader.populate_default_components(config.site)
-      Beacon.Loader.reload_components_module(config.site)
+    task_supervisor = task_supervisor(config.site)
 
-      Beacon.Loader.populate_default_layouts(config.site)
+    # Layouts and pages depend on the components module so we need to load it first
+    Beacon.Loader.populate_default_components(config.site)
+    Beacon.Loader.reload_components_module(config.site)
 
-      # Error pages depend on default layouts
-      Beacon.Loader.populate_default_error_pages(config.site)
+    Beacon.Loader.populate_default_layouts(config.site)
 
-      assets = [
-        Task.Supervisor.async(task_supervisor, fn -> Beacon.Loader.reload_runtime_js(config.site) end),
-        Task.Supervisor.async(task_supervisor, fn -> Beacon.Loader.reload_runtime_css(config.site) end)
-      ]
+    # Error pages depend on default layouts
+    Beacon.Loader.populate_default_error_pages(config.site)
 
-      modules = [
-        Task.Supervisor.async(task_supervisor, fn -> Beacon.Loader.reload_stylesheet_module(config.site) end),
-        Task.Supervisor.async(task_supervisor, fn -> Beacon.Loader.reload_snippets_module(config.site) end),
-        Task.Supervisor.async(task_supervisor, fn -> Beacon.Loader.reload_live_data_module(config.site) end),
-        Task.Supervisor.async(task_supervisor, fn -> Beacon.Loader.reload_layouts_modules(config.site) end),
-        Task.Supervisor.async(task_supervisor, fn -> Beacon.Loader.reload_error_page_module(config.site) end),
-        Task.Supervisor.async(task_supervisor, fn -> Beacon.Loader.reload_pages_modules(config.site, per_page: 20) end)
-        # TODO: load main pages (order_by: path, per_page: 10) to avoid SEO issues
-      ]
+    assets = [
+      Task.Supervisor.async(task_supervisor, fn -> Beacon.Loader.reload_runtime_js(config.site) end),
+      Task.Supervisor.async(task_supervisor, fn -> Beacon.Loader.reload_runtime_css(config.site) end)
+    ]
 
-      Task.await_many(modules, :timer.minutes(2))
+    modules = [
+      Task.Supervisor.async(task_supervisor, fn -> Beacon.Loader.reload_stylesheet_module(config.site) end),
+      Task.Supervisor.async(task_supervisor, fn -> Beacon.Loader.reload_snippets_module(config.site) end),
+      Task.Supervisor.async(task_supervisor, fn -> Beacon.Loader.reload_live_data_module(config.site) end),
+      Task.Supervisor.async(task_supervisor, fn -> Beacon.Loader.reload_layouts_modules(config.site) end),
+      Task.Supervisor.async(task_supervisor, fn -> Beacon.Loader.reload_error_page_module(config.site) end),
+      Task.Supervisor.async(task_supervisor, fn -> Beacon.Loader.reload_pages_modules(config.site, per_page: 20) end)
+      # TODO: load main pages (order_by: path, per_page: 10) to avoid SEO issues
+    ]
 
-      # TODO: revisit this timeout after we upgrade to Tailwind v4
-      Task.await_many(assets, :timer.minutes(5))
-    end
+    Task.await_many(modules, :timer.minutes(2))
 
-    {time, _} = :timer.tc(boot, :seconds)
-    Logger.info("Beacon.Boot finished booting site #{config.site} in #{time} seconds")
+    # TODO: revisit this timeout after we upgrade to Tailwind v4
+    Task.await_many(assets, :timer.minutes(5))
+
+    # TODO: add telemetry to measure booting time
+    Logger.info("Beacon.Boot finished booting site #{config.site}")
 
     :ignore
   end
