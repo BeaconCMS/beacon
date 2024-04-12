8257585298ff7f8641842d3d839ec7a136418bc6
APB9785 <74077809+APB9785@users.noreply.github.com>
2024-04-12 17:02:58 -0500

    Add port, user, pass to test repo config

--- config/test.exs
+++ config/test.exs
@@ -4,12 +4,31 @@ config :phoenix, :json_library, Jason
 
 config :logger, level: :error
 
+db_port = fn
+  "mysql" -> 3306
+  "mssql" -> 1433
+  _ -> 5432
+end
+
+username = fn
+  "mysql" -> nil
+  "mssql" -> nil
+  _ -> "postgres"
+end
+
+password = fn
+  "mysql" -> nil
+  "mssql" -> "Beacon!CMS!!"
+  _ -> "postgres"
+end
+
 config :beacon, Beacon.Repo,
   database: "beacon_test",
-  password: "postgres",
+  password: password.(System.get_env("DB_ADAPTER")),
   pool: Ecto.Adapters.SQL.Sandbox,
-  username: "postgres",
-  ownership_timeout: 1_000_000_000
+  username: username.(System.get_env("DB_ADAPTER")),
+  ownership_timeout: 1_000_000_000,
+  port: db_port.(System.get_env("DB_ADAPTER"))
 
 config :beacon, ecto_repos: [Beacon.Repo]
 
