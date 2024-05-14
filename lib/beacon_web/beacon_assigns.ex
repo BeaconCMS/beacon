5431f619a25b45a14b21b4ad1694a3ed98eece2f
Leandro Pereira <leandro@leandro.io>
2024-05-14 12:10:37 -0400

    make Template__render__ more reliable by reusing the same data as the LivePage

--- lib/beacon_web/beacon_assigns.ex
+++ lib/beacon_web/beacon_assigns.ex
@@ -28,6 +28,33 @@ defmodule BeaconWeb.BeaconAssigns do
             }
 
   @doc false
+  def build(site) when is_atom(site), do: %__MODULE__{site: site}
+
+  def build(beacon_assigns = %__MODULE__{}, path_info, query_params) when is_list(path_info) and is_map(query_params) do
+    site = beacon_assigns.site
+    %{site: ^site} = page = Beacon.RouterServer.lookup_page!(beacon_assigns.site, path_info)
+    components_module = Beacon.Loader.Components.module_name(site)
+    page_module = Beacon.Loader.Page.module_name(site, page.id)
+    live_data = BeaconWeb.DataSource.live_data(site, path_info, Map.drop(query_params, ["path"]))
+    page_title = BeaconWeb.DataSource.page_title(site, page.id, live_data)
+
+    %{
+      beacon_assigns
+      | query_params: query_params,
+        page: %{path: page.path, title: page_title},
+        private: %{
+          live_data_keys: Map.keys(live_data),
+          live_path: path_info,
+          layout_id: page.layout_id,
+          page_id: page.id,
+          page_updated_at: DateTime.utc_now(),
+          page_module: page_module,
+          components_module: components_module
+        }
+    }
+  end
+
+  @doc false
   def update(_socket_or_assigns, _key, _value)
 
   def update(%{assigns: %{beacon: _beacon}} = socket, key, value) do
