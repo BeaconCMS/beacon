77bfd18c22103b4ed898d09e4777919c9eeacb85
Leandro Pereira <leandro@leandro.io>
2024-09-27 12:17:14 -0400

    Remove unnecessary :plug_cowboy dep
    https://github.com/BeaconCMS/beacon_live_admin/pull/262

--- mix.exs
+++ mix.exs
@@ -81,8 +81,8 @@ defmodule Beacon.MixProject do
 
       # Dev, Test, Docs
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:bandit, "~> 1.0", only: :dev, optional: true},
       {:phoenix_view, "~> 2.0", only: [:dev, :test]},
-      {:plug_cowboy, "~> 2.6", only: [:dev, :test]},
       {:ex_doc, "~> 0.29", only: :dev, runtime: false},
       {:esbuild, "~> 0.5", only: :dev},
       {:phoenix_live_reload, "~> 1.3", only: :dev},
