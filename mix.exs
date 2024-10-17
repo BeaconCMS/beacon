ddf3be4b57fd68f0482e2356fae53406a69107b6
Leandro Pereira <leandro@leandro.io>
2024-10-17 17:13:50 -0400

    Unlock live_monaco_editor
    Move it to beacon_live_admin

--- mix.exs
+++ mix.exs
@@ -54,7 +54,6 @@ defmodule Beacon.MixProject do
       # Overridable
       override_dep(:phoenix, "~> 1.7", "PHOENIX_VERSION", "PHOENIX_PATH"),
       override_dep(:phoenix_live_view, "~> 0.20", "PHOENIX_LIVE_VIEW_VERSION", "PHOENIX_LIVE_VIEW_PATH"),
-      override_dep(:live_monaco_editor, "~> 0.1", "LIVE_MONACO_EDITOR_VERSION", "LIVE_MONACO_EDITOR_PATH"),
       override_dep(:mdex, "~> 0.2", "MDEX_VERSION", "MDEX_PATH"),
 
       # Runtime
