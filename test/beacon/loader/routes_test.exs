847666b5a59c3a48d3c3ae49636346e068bb8365
APB9785 <74077809+APB9785@users.noreply.github.com>
2024-10-18 12:55:55 -0500

    don't import, test locally instead

--- test/beacon/loader/routes_test.exs
+++ test/beacon/loader/routes_test.exs
@@ -2,38 +2,39 @@ defmodule Beacon.Loader.RoutesTest do
   use Beacon.DataCase, async: true
   use Beacon.Test
 
-  @before_compile Beacon.ErrorHandlerCompileHook
+  @router_module :"Elixir.Beacon.Web.LiveRenderer.fb13425603d2684189757bc0a91e1833.Routes"
+
+  setup do
+    Process.flag(:error_handler, Beacon.ErrorHandler)
+    :ok
+  end
 
   test "beacon_asset_path" do
-    import :"Elixir.Beacon.Web.LiveRenderer.fb13425603d2684189757bc0a91e1833.Routes"
-    assert beacon_asset_path("logo.webp") == "/__beacon_assets__/booted/logo.webp"
+    assert @router_module.beacon_asset_path("logo.webp") == "/__beacon_assets__/booted/logo.webp"
   end
 
   test "beacon_asset_url" do
-    import :"Elixir.Beacon.Web.LiveRenderer.fb13425603d2684189757bc0a91e1833.Routes"
-    assert beacon_asset_url("logo.webp") == "http://localhost:4000/__beacon_assets__/booted/logo.webp"
+    assert @router_module.beacon_asset_url("logo.webp") == "http://localhost:4000/__beacon_assets__/booted/logo.webp"
   end
 
   describe "sigil_p" do
     test "static" do
-      import :"Elixir.Beacon.Web.LiveRenderer.fb13425603d2684189757bc0a91e1833.Routes"
-      assert ~p"/" == "/nested/site"
-      assert ~p"/contact" == "/nested/site/contact"
+      assert @router_module.sigil_p("/") == "/nested/site"
+      assert @router_module.sigil_p("/contact") == "/nested/site/contact"
     end
 
     test "derive path from page" do
-      import :"Elixir.Beacon.Web.LiveRenderer.fb13425603d2684189757bc0a91e1833.Routes"
       page = beacon_page_fixture(site: :booted, path: "/elixir-lang")
-      assert ~p"/#{page}" == "/nested/site/elixir-lang"
-      assert ~p"/posts/#{page}" == "/nested/site/posts/elixir-lang"
+
+      assert @router_module.sigil_p("/#{page}") == "/nested/site/elixir-lang"
+      assert @router_module.sigil_p("/posts/#{page}") == "/nested/site/posts/elixir-lang"
     end
 
     test "with dynamic segments" do
-      import :"Elixir.Beacon.Web.LiveRenderer.fb13425603d2684189757bc0a91e1833.Routes"
       page = %{id: 1}
-      assert ~p|/posts/#{page.id}| == "/nested/site/posts/1"
 
-      assert ~p|/posts/#{"a b"}| == "/nested/site/posts/a%20b"
+      assert @router_module.sigil_p("/posts/#{page.id}") == "/nested/site/posts/1"
+      assert @router_module.sigil_p("/posts/#{"a b"}") == "/nested/site/posts/a%20b"
     end
   end
 end
