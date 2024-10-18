e500cbbe6ac0335b4f4827ef494805f2e62af04b
APB9785 <74077809+APB9785@users.noreply.github.com>
2024-10-18 12:41:37 -0500

    inside tests?

--- test/beacon/loader/routes_test.exs
+++ test/beacon/loader/routes_test.exs
@@ -4,30 +4,34 @@ defmodule Beacon.Loader.RoutesTest do
 
   setup do
     Process.flag(:error_handler, Beacon.ErrorHandler)
-    import :"Elixir.Beacon.Web.LiveRenderer.fb13425603d2684189757bc0a91e1833.Routes"
   end
 
   test "beacon_asset_path" do
+    import :"Elixir.Beacon.Web.LiveRenderer.fb13425603d2684189757bc0a91e1833.Routes"
     assert beacon_asset_path("logo.webp") == "/__beacon_assets__/booted/logo.webp"
   end
 
   test "beacon_asset_url" do
+    import :"Elixir.Beacon.Web.LiveRenderer.fb13425603d2684189757bc0a91e1833.Routes"
     assert beacon_asset_url("logo.webp") == "http://localhost:4000/__beacon_assets__/booted/logo.webp"
   end
 
   describe "sigil_p" do
     test "static" do
+      import :"Elixir.Beacon.Web.LiveRenderer.fb13425603d2684189757bc0a91e1833.Routes"
       assert ~p"/" == "/nested/site"
       assert ~p"/contact" == "/nested/site/contact"
     end
 
     test "derive path from page" do
+      import :"Elixir.Beacon.Web.LiveRenderer.fb13425603d2684189757bc0a91e1833.Routes"
       page = beacon_page_fixture(site: :booted, path: "/elixir-lang")
       assert ~p"/#{page}" == "/nested/site/elixir-lang"
       assert ~p"/posts/#{page}" == "/nested/site/posts/elixir-lang"
     end
 
     test "with dynamic segments" do
+      import :"Elixir.Beacon.Web.LiveRenderer.fb13425603d2684189757bc0a91e1833.Routes"
       page = %{id: 1}
       assert ~p|/posts/#{page.id}| == "/nested/site/posts/1"
 
