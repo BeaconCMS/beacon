dfe0c957b4a195490dd63e63d62e863e03e81f46
Leandro Pereira <leandro@leandro.io>
2024-10-23 13:11:31 -0400

    Content/Component - validate attr opts and slot opts (#618)

--- lib/beacon/content.ex
+++ lib/beacon/content.ex
@@ -2948,7 +2948,14 @@ defmodule Beacon.Content do
         component
 
       {:error, changeset} ->
-        raise "failed to create component: #{inspect(changeset.errors)}"
+        errors =
+          Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} ->
+            Regex.replace(~r"%{(\w+)}", msg, fn _, key ->
+              opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
+            end)
+          end)
+
+        raise "failed to create component: #{inspect(errors)}"
     end
   end
 
