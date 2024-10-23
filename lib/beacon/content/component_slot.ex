fae35440c45d4a95c42756fc9cd3147b79cf89f7
Leandro Pereira <leandro@leandro.io>
2024-10-23 13:05:33 -0400

    credo

--- lib/beacon/content/component_slot.ex
+++ lib/beacon/content/component_slot.ex
@@ -56,13 +56,11 @@ defmodule Beacon.Content.ComponentSlot do
     opts = get_field(changeset, :opts) |> maybe_binary_to_term()
     not_allowed = Keyword.keys(opts) -- [:required, :validate_attrs, :doc]
 
-    cond do
-      Enum.count(not_allowed) > 0 ->
-        name = get_field(changeset, :name)
-        add_error(changeset, :opts, "invalid opts for slot #{inspect(name)}: #{inspect(not_allowed)}")
-
-      true ->
-        changeset
+    if Enum.count(not_allowed) > 0 do
+      name = get_field(changeset, :name)
+      add_error(changeset, :opts, "invalid opts for slot #{inspect(name)}: #{inspect(not_allowed)}")
+    else
+      changeset
     end
   end
 
