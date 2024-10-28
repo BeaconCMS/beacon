5bc6349dbcaa8ebfe2281ec69276321af7541e02
Anushka <anushka@dbadbadba.com>
2024-10-28 21:18:43 +0530

    exclude the node modules from tailwind content

--- lib/beacon/tailwind_compiler.ex
+++ lib/beacon/tailwind_compiler.ex
@@ -268,7 +268,8 @@ defmodule Beacon.RuntimeCSS.TailwindCompiler do
     './assets/js/**/*.js',
     './lib/*_web.ex',
     './lib/*_web/**/*.*ex',
-    './apps/*_web/assets/**/*.js',
+    './apps/*_web/assets/js/**/*.js',
+    '!./apps/*_web/assets/node_modules/**',
     './apps/*_web/lib/*_web.ex',
     './apps/*_web/lib/*_web/**/*.*ex',
     '#{tmp_dir}/*.template'
