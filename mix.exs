ef0f8949452e8389eec36e6f2e553ce2905f3ed4
Leandro Pereira <leandro@leandro.io>
2024-10-31 17:40:01 -0400

    v0.1.4

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Beacon.MixProject do
   use Mix.Project
 
-  @version "0.1.3"
+  @version "0.1.4"
   @source_url "https://github.com/BeaconCMS/beacon"
   @homepage_url "https://beaconcms.org"
 
