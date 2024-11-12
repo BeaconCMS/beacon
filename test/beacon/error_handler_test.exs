cb7a35e39826138aba2ffcdd461349d4641b9c13
Leandro Pereira <leandro@leandro.io>
2024-11-12 10:18:08 -0500

    chore: add more error handler tests

--- test/beacon/error_handler_test.exs
+++ test/beacon/error_handler_test.exs
@@ -1,7 +1,12 @@
 defmodule Beacon.ErrorHandlerTest do
   use ExUnit.Case
 
-  test "error handler" do
+  test "enable" do
+    Beacon.ErrorHandler.enable(:my_site)
+    assert Process.info(self(), :error_handler) == {:error_handler, Beacon.ErrorHandler}
+  end
+
+  test "raise on undefined resources" do
     Beacon.ErrorHandler.enable(:nosite)
 
     assert_raise UndefinedFunctionError, fn ->
