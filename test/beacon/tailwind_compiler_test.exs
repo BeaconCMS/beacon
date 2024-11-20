194efffa9c827dad900ec1d14322d632661b7959
Leandro Pereira <leandro@leandro.io>
2024-11-20 12:40:22 -0500

    chore: fix tests

--- test/beacon/tailwind_compiler_test.exs
+++ test/beacon/tailwind_compiler_test.exs
@@ -57,10 +57,6 @@ defmodule Beacon.RuntimeCSS.TailwindCompilerTest do
     :ok
   end
 
-  test "config" do
-    assert TailwindCompiler.config(default_site()) =~ "export default"
-  end
-
   test "css" do
     assert TailwindCompiler.css(default_site()) =~ ".custom-font-style { @apply font-sans; color: #5e5e5e }"
   end
