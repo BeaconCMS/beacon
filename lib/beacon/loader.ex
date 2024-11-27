4ac3b77f907c43fdc03c657df83f5bd216630cbe
Leandro Pereira <leandro@leandro.io>
2024-11-27 13:58:00 -0500

    cleanup

--- lib/beacon/loader.ex
+++ lib/beacon/loader.ex
@@ -380,11 +380,9 @@ defmodule Beacon.Loader do
     {:noreply, config}
   end
 
-  # def handle_info(msg, config) do
-  #   dbg(msg)
-
-  #   raise inspect(msg)
-  #   Logger.warning("Beacon.Loader can not handle the message: #{inspect(msg)}")
-  #   {:noreply, config}
-  # end
+  def handle_info(msg, config) do
+    raise inspect(msg)
+    Logger.warning("Beacon.Loader can not handle the message: #{inspect(msg)}")
+    {:noreply, config}
+  end
 end
