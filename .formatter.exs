94fab1105d80337c09cc3460447d23055f827468
Leandro Pereira <leandro@leandro.io>
2024-12-09 09:56:42 -0500

    chore: disable auto curly braces
    we still support LV 0.20

--- .formatter.exs
+++ .formatter.exs
@@ -7,6 +7,7 @@ locals_without_parens = [
   import_deps: [:ecto, :ecto_sql, :phoenix],
   line_length: 150,
   plugins: [Phoenix.LiveView.HTMLFormatter],
+  migrate_eex_to_curly_interpolation: false,
   inputs: ["{mix,.formatter,dev}.exs", "{config,lib,test}/**/*.{heex,ex,exs}"],
   locals_without_parens: locals_without_parens,
   export: [locals_without_parens: locals_without_parens]
