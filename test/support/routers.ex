e76ef6d919d4e19f9364b3c46cdf5129f4c8472f
Leandro Pereira <leandro@leandro.io>
2024-12-11 15:04:44 -0500

    fix: do not crash boot when routes are missing (#701)

--- test/support/routers.ex
+++ test/support/routers.ex
@@ -2,6 +2,39 @@ defmodule Beacon.BeaconTest.NoRoutesRouter do
   use Beacon.BeaconTest.Web, :router
 end
 
+defmodule Beacon.BeaconTest.ReachTestRouter do
+  use Beacon.BeaconTest.Web, :router
+  use Beacon.Router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  scope path: "/host_test", host: "host.com" do
+    pipe_through :browser
+    beacon_site "/", site: :host_test
+  end
+
+  scope path: "/conflict" do
+    pipe_through :browser
+    live "/:page", Beacon.BeaconTest.LiveViews.FooBarLive
+    beacon_site "/", site: :not_booted
+  end
+
+  scope path: "/my_site" do
+    pipe_through :browser
+    beacon_site "/", site: :my_site
+  end
+
+  scope path: "/other" do
+    pipe_through :browser
+  end
+end
+
 defmodule Beacon.BeaconTest.Router do
   use Beacon.BeaconTest.Web, :router
   use Beacon.Router
@@ -18,20 +51,6 @@ defmodule Beacon.BeaconTest.Router do
     pipe_through :browser
     beacon_site "/site", site: :booted
     beacon_site "/media", site: :s3_site
-  end
-
-  scope "/" do
-    pipe_through :browser
-
-    live_session :default do
-      live "/", Beacon.BeaconTest.LiveViews.FooBarLive
-      live "/nested/:page", Beacon.BeaconTest.LiveViews.FooBarLive
-    end
-  end
-
-  scope path: "/", host: "host.com" do
-    pipe_through :browser
-    beacon_site "/", site: :host_test
   end
 
   # `alias` is not really used but is present here to verify that `beacon_site` has no conflicts with custom aliases
