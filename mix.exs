4e7fc39b6682f4bc8c3633afa4cd43a4fe690fdb
Leandro Pereira <leandro@leandro.io>
2025-01-02 15:20:25 -0500

    chore: bump igniter v0.5

--- mix.exs
+++ mix.exs
@@ -79,7 +79,7 @@ defmodule Beacon.MixProject do
       {:safe_code, "~> 0.2"},
       {:solid, "~> 0.14"},
       {:tailwind, "~> 0.2"},
-      {:igniter, "~> 0.4"},
+      {:igniter, "~> 0.5"},
 
       # Dev, Test, Docs
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
