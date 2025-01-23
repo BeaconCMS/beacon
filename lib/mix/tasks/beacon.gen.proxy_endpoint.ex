c7e9bb77d3d48d8a17d5d8cd16301240d5e02e59
Leandro Pereira <leandro@leandro.io>
2025-01-23 16:41:57 -0500

    fix: ignore endpoints without socket call

--- lib/mix/tasks/beacon.gen.proxy_endpoint.ex
+++ lib/mix/tasks/beacon.gen.proxy_endpoint.ex
@@ -190,10 +190,15 @@ defmodule Mix.Tasks.Beacon.Gen.ProxyEndpoint do
         end
       )
       |> Igniter.Project.Module.find_and_update_module!(endpoint, fn zipper ->
-        {:ok, zipper} = Igniter.Code.Function.move_to_function_call(zipper, :socket, 3)
-        # TODO replace the node with a commented-out version of itself
-        # blocked by https://github.com/ash-project/igniter/pull/200
-        {:ok, Sourceror.Zipper.remove(zipper)}
+        case Igniter.Code.Function.move_to_function_call(zipper, :socket, 3) do
+          {:ok, zipper} ->
+            # TODO replace the node with a commented-out version of itself
+            # blocked by https://github.com/ash-project/igniter/pull/200
+            {:ok, Sourceror.Zipper.remove(zipper)}
+
+          _ ->
+            {:ok, zipper}
+        end
       end)
       |> Igniter.Project.Module.find_and_update_module!(endpoint, fn zipper ->
         case Beacon.Igniter.move_to_constant(zipper, :session_options) do
