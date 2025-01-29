75cf64b812bdada069c82e2771c46f53f6c9e0b4
APB9785 <74077809+APB9785@users.noreply.github.com>
2025-01-29 16:03:25 -0600

    refactor runtime js

--- lib/beacon/runtime_js.ex
+++ lib/beacon/runtime_js.ex
@@ -2,7 +2,6 @@ defmodule Beacon.RuntimeJS do
   # Runtime compilation and processing of JS files.
   @moduledoc false
   alias Beacon.Content
-  alias Beacon.Content.JSHook
 
   # merge beacon js with host application dependencies js
   # similar to https://github.com/phoenixframework/phoenix_live_dashboard/blob/9140f56c34201237f0feeeff747528eed2795c0c/lib/phoenix/live_dashboard/controllers/assets.ex#L6-L11
@@ -27,64 +26,40 @@ defmodule Beacon.RuntimeJS do
         ]
       end
 
-    # TODO: delete tmp files after
     tmp_dir = tmp_dir!()
-    cmd_opts = [cd: File.cwd!(), stderr_to_stdout: true]
 
-    {hooks, imports} =
-      Enum.reduce(Content.list_js_hooks(site), {[], []}, fn hook, {hooks, imports} = acc ->
-        # TODO: handle errors (invalid json or more than one export, it should have a single export)
-        export =
-          with meta when not is_nil(meta) <- JSHook.get_code_metadata(hook, tmp_dir),
-               {_, meta} <- Enum.at(meta["outputs"] || %{}, 0),
-               [export] <- meta["exports"] do
-            export
-          else
-            _ -> nil
-          end
-
+    {names, imports, paths} =
+      Enum.reduce(Content.list_js_hooks(site), {[], [], []}, fn hook, {names, imports, paths} = acc ->
+        # Write a file for each hook
         hook_js_path = Path.join(tmp_dir, hook.name <> ".js")
+        File.write!(hook_js_path, hook.code)
 
-        import =
-          cond do
-            export == "default" ->
-              "import #{hook.name} from '#{hook_js_path}';"
-
-            is_binary(export) ->
-              "import { #{export} as #{hook.name} } from '#{hook_js_path}';"
-
-            :else ->
-              nil
+        # Don't cleanup these files yet, we'll cleanup altogether at the end
+        export =
+          case get_export(hook, dir: tmp_dir, cleanup: false) do
+            {:ok, export} when export in ["default", hook.name] -> export
+            {:error, _error} -> nil
           end
 
-        if import do
-          {[hook.name | hooks], [import | imports]}
+        import_code =
+          cond do
+            export == "default" -> "import #{hook.name} from '#{hook_js_path}';"
+            is_binary(export) -> "import { #{export} as #{hook.name} } from '#{hook_js_path}';"
+            :else -> nil
+          end
+
+        if import_code do
+          {[hook.name | names], [import_code | imports], [hook_js_path | paths]}
         else
           # TODO: warn? error?
           acc
         end
       end)
 
-    hooks = [
-      Enum.intersperse(imports, "\n"),
-      "\n",
-      "export default {\n",
-      Enum.intersperse(hooks, ",\n"),
-      "\n}"
-    ]
-
-    IO.puts(hooks)
-
+    # With all the valid data accumulated, write a single file for beacon hook imports
+    # and load it into memory to be injected into the existing js deps
     hooks_js_path = Path.join(tmp_dir, "hooks.js")
-    File.write!(hooks_js_path, hooks)
-
-    # TODO: minify on/off (--minify)
-    args = ~w(#{hooks_js_path} --bundle --format=iife --target=es2016 --platform=browser --global-name=BeaconHooks --log-level=error)
-
-    # TODO: check if esbuild bin exist, similar to TailwindCompiler
-    # TODO: copy esbuild bin into the release
-    # TODO: handle errors (exit != 0)
-    {hooks, 0} = System.cmd(Esbuild.bin_path(), args, cmd_opts)
+    hooks = build_beacon_hooks(hooks_js_path, imports, names, minify?)
 
     js_deps =
       assets
@@ -95,7 +70,37 @@ defmodule Beacon.RuntimeJS do
         |> String.replace("//# sourceMappingURL=", "// ")
       end)
 
+    # Everything we need is in memory now, so we can cleanup the files
+    cleanup([hooks_js_path | paths], tmp_dir)
+
     IO.iodata_to_binary([hooks, "\n", js_deps])
+  end
+
+  defp build_beacon_hooks(path, imports, names, minify?) do
+    hooks_code = [
+      Enum.intersperse(imports, "\n"),
+      "\n",
+      "export default {\n",
+      Enum.intersperse(names, ",\n"),
+      "\n}"
+    ]
+
+    File.write!(path, hooks_code)
+
+    args =
+      ~w(#{path} --bundle --format=iife --target=es2016 --platform=browser) ++
+        ~w(--global-name=BeaconHooks --log-level=error) ++
+        if(minify?, do: ~w(--minify), else: [])
+
+    cmd_opts = [cd: File.cwd!(), stderr_to_stdout: true]
+
+    # TODO: check if esbuild bin exist, similar to TailwindCompiler
+    # TODO: copy esbuild bin into the release
+    # TODO: handle errors (exit != 0)
+    case System.cmd(Esbuild.bin_path(), args, cmd_opts) do
+      {hooks, 0} -> hooks
+      _ -> []
+    end
   end
 
   defp tmp_dir! do
@@ -145,5 +150,58 @@ defmodule Beacon.RuntimeJS do
       [[hash]] -> hash
       _ -> nil
     end
+  end
+
+  def get_export(hook, opts \\ []) do
+    dir = Keyword.get(opts, :dir, tmp_dir!())
+    cleanup? = Keyword.get(opts, :cleanup, true)
+
+    hook_js_path = Path.join(dir, hook.name <> ".js")
+
+    if !File.exists?(hook_js_path), do: File.write!(hook_js_path, hook.code)
+
+    with %{"outputs" => %{} = outputs} <- get_code_metadata(hook, dir),
+         {_, %{"exports" => exports}} <- Enum.at(outputs, 0) do
+      if cleanup?, do: cleanup([hook_js_path], dir)
+
+      case exports do
+        [] -> {:error, :no_export}
+        [export] -> {:ok, export}
+        exports when is_list(exports) -> {:error, :multiple_exports}
+        _ -> {:error, :unknown}
+      end
+    else
+      _ ->
+        if cleanup?, do: cleanup([hook], dir)
+        {:error, :esbuild_failed}
+    end
+  end
+
+  defp get_code_metadata(hook, dir) do
+    hook_js_path = Path.join(dir, hook.name <> ".js")
+    meta_json_path = Path.join(dir, hook.name <> ".json")
+    meta_out_js_path = Path.join(dir, hook.name <> "_meta.js")
+    cmd_opts = [cd: File.cwd!(), stderr_to_stdout: true]
+
+    {_, 0} = System.cmd(Esbuild.bin_path(), ~w(#{hook_js_path} --metafile=#{meta_json_path} --outfile=#{meta_out_js_path}), cmd_opts)
+
+    with {:ok, meta} <- File.read(meta_json_path),
+         {:ok, meta} <- Jason.decode(meta) do
+      cleanup([meta_json_path, meta_out_js_path])
+      meta
+    else
+      _ ->
+        cleanup([meta_json_path, meta_out_js_path])
+        nil
+    end
+  end
+
+  defp cleanup(files) when is_list(files) do
+    Enum.each(files, &File.rm/1)
+  end
+
+  defp cleanup(files, dir) when is_list(files) do
+    Enum.each(files, &File.rm/1)
+    File.rmdir(dir)
   end
 end
