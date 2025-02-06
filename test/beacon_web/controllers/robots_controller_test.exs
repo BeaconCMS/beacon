a93854d8322bc8a8a072b1aeba881810b0224e1a
Leandro Pereira <leandro@leandro.io>
2025-02-06 16:43:29 -0500

    fix robots test

--- test/beacon_web/controllers/robots_controller_test.exs
+++ test/beacon_web/controllers/robots_controller_test.exs
@@ -7,18 +7,7 @@ defmodule Beacon.Web.RobotsControllerTest do
     assert response(conn, 200) == """
            # http://www.robotstxt.org
            User-agent: *
-           Sitemap: http://site_a.com/sitemap.xml
-           """
-
-    assert response_content_type(conn, :txt) =~ "charset=utf-8"
-
-    # site: :not_booted
-    conn = get(conn, "/other/robots.txt")
-
-    assert response(conn, 200) == """
-           # http://www.robotstxt.org
-           User-agent: *
-           Sitemap: http://site_a.com/other/sitemap.xml
+           Sitemap: http://site_a.com/sitemap_index.xml
            """
 
     assert response_content_type(conn, :txt) =~ "charset=utf-8"
