4ba67490a21ec85768631ead231ef73c5302b0fd
Leandro Pereira <leandro@leandro.io>
2025-03-04 10:53:30 -0500

    docs

--- lib/mix/tasks/beacon.gen.site.ex
+++ lib/mix/tasks/beacon.gen.site.ex
@@ -26,11 +26,39 @@ defmodule Mix.Tasks.Beacon.Gen.Site.Docs do
     mix beacon.gen.site --site my_site --path / --host mysite.com
     ```
 
-    To define a custom host to work locally and thhe production host:
+    To define a custom host to work locally and the production host:
 
     ```bash
     mix beacon.gen.site --site my_site --path / --host-dev local.mysite.com --host mysite.com
     ```
+
+    ## Using --host-dev for Multiple Sites
+
+    The `--host-dev` option is particularly useful when you need to run multiple sites at the root path (`/`). Without custom hosts, all sites would try to serve at `localhost:4000/`, which would cause conflicts.
+
+    For example, if you have two sites:
+
+    ```bash
+    mix beacon.gen.site --site blog --path / --host-dev local.blog.mysite.com --host blog.mysite.com
+    mix beacon.gen.site --site shop --path / --host-dev local.shop.mysite.com --host shop.mysite.com
+    ```
+
+    To make this work locally, you have two options:
+
+    1. Edit your `/etc/hosts` file (or equivalent) to add:
+       ```
+       127.0.0.1 local.blog.mysite.com
+       127.0.0.1 local.shop.mysite.com
+       ```
+
+       This is the simplest solution and works well for local development. Locally in the dev environment, sites would be accessible at:
+       - `http://local.blog.mysite.com:4000/`
+       - `http://local.shop.mysite.com:4000/`
+
+    2. Use a local development tunneling service. Some options:
+       - [ngrok](https://ngrok.com/)
+       - [localtunnel](https://localtunnel.github.io/www/)
+       - [cloudflare tunnel](https://developers.cloudflare.com/cloudflare-one/connections/connect-apps/install-and-setup/tunnel-guide/local/)
 
     ## Options
 
