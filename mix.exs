39f88dcbe2fa9ac36e801a83001fcfc814cc6e33
Leandro Pereira <leandro@leandro.io>
2025-04-07 11:07:35 -0400

    chore: support visual editor component (#813)

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Beacon.MixProject do
   use Mix.Project
 
-  @version "0.5.1"
+  @version "0.5.2-dev"
   @source_url "https://github.com/BeaconCMS/beacon"
   @homepage_url "https://beaconcms.org"
 
