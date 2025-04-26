001e533eae624d7aaafbf6ae719080354d25734d
javierg <tijuanate@gmail.com>
2025-04-26 08:51:26 -0700

    Fix:#821 Supports private phoenix_live_view session as tuple and map

--- lib/beacon/web/controllers/error_html.ex
+++ lib/beacon/web/controllers/error_html.ex
@@ -8,7 +8,7 @@ defmodule Beacon.Web.ErrorHTML do
 
   @doc false
   def render(<<status_code::binary-size(3), _rest::binary>> = template, %{conn: conn}) do
-    {_, _, %{extra: %{session: %{"beacon_site" => site}}}} = conn.private.phoenix_live_view
+    site = conn.private.phoenix_live_view |> fetch_session_site()
     error_module = Beacon.Loader.fetch_error_page_module(site)
     conn = Plug.Conn.assign(conn, :beacon, Beacon.Web.BeaconAssigns.new(site))
     Beacon.apply_mfa(site, error_module, :render, [conn, String.to_integer(status_code)])
@@ -28,5 +28,12 @@ defmodule Beacon.Web.ErrorHTML do
   def render(template, _assigns) do
     Logger.warning("could not find an error page for #{template}, fallbacking to default Phoenix error page")
     Phoenix.Controller.status_message_from_template(template)
+  end
+
+  defp fetch_session_site({_, _, %{extra: %{session: site_session}}}) do
+    case site_session do
+      %{"beacon_site" => site} -> site
+      {Beacon.Router, :session, [site, _]} -> site
+    end
   end
 end
